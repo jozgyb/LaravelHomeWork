989f03c1167377bd109a6a4ff3662f158671e036
Bence <jzsab1@gmail.com>
2021-05-12 19:30:12 +0200

    add auth to gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<h1>Style-1:</h1>
-<div align="right">
-    <a href="{{ route('gallery.create') }}" class="btn btn-primary">Add +</a>
-</div>
+@auth
+<?php if (Auth::user()->role==1 || Auth::user()->role==2){?>
+    <div align="right">
+        <a href="{{ route('gallery.create') }}" class="btn btn-primary">Add +</a>
+    </div>
+<?php } ?>
+@endauth
 <div class="row">
     @foreach($images as $row)
         <div class="col-md-6 col-lg-4 col-xl-3">
@@ -13,6 +16,8 @@
           <div class="caption">
             <p><strong>Title: </strong>{{ $row->title}}</p>
           </div>
+        @auth
+        <?php if (Auth::user()->role==1 || Auth::user()->role==2){?>
           <form action="{{ URL::route('gallery.destroy',$row->id) }}" method="POST">
             <a href="{{ route('gallery.show', $row->id) }}" class="btn btn-success">Show </a>
             <a href="{{ route('gallery.edit', $row->id) }}" class="btn btn-info">Edit </a>
@@ -21,36 +26,9 @@
             <button class="btn btn-danger">Delete </button>
           </form>
         </div>
+        <?php } ?>
+        @endauth
     @endforeach
 </div>
-{{ $images->links('pagination::bootstrap-4') }}
-
-<h1>Style-2:</h1>
-<div align="right">
-    <a href="{{ route('gallery.create') }}" class="btn btn-primary">Add +</a>
-</div>
-<table class="table table-bordered table-striped">
-    <tr>
-        <th width="10%">Image</th>
-        <th width="30%">Title</th>
-        <th width="20%">Action</th>
-    </tr>
-    @foreach($images as $row)
-    <tr>
-        <td ><a href="{{ route('gallery.show', $row->id) }}"><img src="{{ URL::to('/') }}/images/{{ $row->image }}"  width="100" /></a></td>
-        <td>{{ $row->title }}</td>
-        <td>
-        <form action="{{ URL::route('gallery.destroy',$row->id) }}" method="POST">
-          <a href="{{ route('gallery.show', $row->id) }}" class="btn btn-success">Show </a>
-          <a href="{{ route('gallery.edit', $row->id) }}" class="btn btn-info">Edit </a>
-          <input type="hidden" name="_method" value="DELETE">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <button class="btn btn-danger">Delete </button>
-        </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
 {{ $images->links('pagination::bootstrap-4') }}
 @endsection
-
